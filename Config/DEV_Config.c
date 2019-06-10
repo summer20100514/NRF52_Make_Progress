8d37652e1d65953607787e1fbe1907bf5d7958a8
summer <1032755103@qq.com>
2019-06-10 22:05:46 +0800

    make progress fisrt init

---
+++ Config/DEV_Config.c
@@ -0,0 +1,83 @@
+/*****************************************************************************
+* | File      	:   DEV_Config.c
+* | Author      :   Waveshare team
+* | Function    :   Hardware underlying interface
+* | Info        :
+*                Used to shield the underlying layers of each master 
+*                and enhance portability
+*----------------
+* |	This version:   V2.0
+* | Date        :   2018-10-30
+* | Info        :
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documnetation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to  whom the Software is
+# furished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+******************************************************************************/
+#include "DEV_Config.h"
+//#include "stm32f1xx_hal_spi.h"
+#include "nrf_drv_spi.h"
+#include "app_util_platform.h"
+#include "nrf_gpio.h"
+#include "nrf_delay.h"
+#include "nrf_log.h"
+#include "boards.h"
+#include "app_error.h"
+
+#define SPI_INSTANCE  0 /**< SPI instance index. */
+static const nrf_drv_spi_t spi = NRF_DRV_SPI_INSTANCE(SPI_INSTANCE);  /**< SPI instance. */
+static volatile bool spi_xfer_done;  /**< Flag used to indicate that SPI instance completed the transfer. */
+
+/**
+ * @brief SPI user event handler.
+ * @param event
+ */
+void spi_event_handler(nrf_drv_spi_evt_t const * p_event,
+                       void *                    p_context)
+{
+    spi_xfer_done = true;
+    // NRF_LOG_RAW_INFO("*");
+}
+
+void spi_and_gpio_init(void)
+{
+    nrf_gpio_cfg_input(EPD_BUSY_PIN, NRF_GPIO_PIN_NOPULL);
+    //nrf_gpio_cfg_input(EPD_BUSY_PIN, NRF_GPIO_PIN_PULLUP);
+    nrf_gpio_cfg_output(EPD_DC_PIN);
+
+    nrf_drv_spi_config_t spi_config = NRF_DRV_SPI_DEFAULT_CONFIG;
+    spi_config.ss_pin   = EPD_CS_PIN;
+    spi_config.miso_pin = NRFX_SPI_PIN_NOT_USED;
+    spi_config.mosi_pin = EPD_MOSI_PIN;
+    spi_config.sck_pin  = EPD_CLK_PIN;
+    APP_ERROR_CHECK(nrf_drv_spi_init(&spi, &spi_config, spi_event_handler, NULL));
+}
+
+//extern SPI_HandleTypeDef hspi1;
+void DEV_SPI_WriteByte(UBYTE value)
+{
+    //HAL_SPI_Transmit(&hspi1, &value, 1, 1000);
+    spi_xfer_done = false;
+
+    APP_ERROR_CHECK(nrf_drv_spi_transfer(&spi, &value, 1, NULL, 0));
+    while (!spi_xfer_done)
+    {
+        __WFE();
+    }
+}
+
+
---
+++ rotary_encoder.h
@@ -0,0 +1,33 @@
+#ifndef __ROTARY_ENCODER_H_
+#define __ROTARY_ENCODER_H_
+
+#include <stdint.h>
+ 
+enum {
+    ROTARY_STATE_INVALID,
+    ROTARY_STATE_CLOCKWISE,
+    ROTARY_STATE_COUNTERCLOCKWISE,
+};
+
+enum {
+    KEY_UNPRESSED,
+    KEY_PRESSED_SHORT,
+    KEY_PRESSED_LONG
+};
+
+#define ROTARY_ENCODER_PIN_A      3
+#define ROTARY_ENCODER_PIN_B      2
+#define ROTARY_ENCODER_PIN_KEY    4
+
+#define PULSE_CNT_THRESHOLD       2
+
+#define KEY_PRESS_SHORT_TIMEOUT   60
+#define KEY_PRESS_LONG_TIMEOUT    1000
+
+void rotary_encoder_init(void);
+int get_rotary_encoder_state(void);
+int get_key_value(void);
+
+#endif
+
+
